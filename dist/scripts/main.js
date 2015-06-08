40508d2a9f762dadfbe0e427da44893fbbbe33e6
Joshua Lagrange <jlagrange87@gmail.com>
2015-06-08 13:52:22 -0500

    normal mode done

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,18 +1,53 @@
 var user = new UserModel();
-var App = Backbone.Router.extend({
-	routes: {
-		'': 'profile',
-		'edit': 'edit'
-	},
-	profile: function() {
-		$('.page').hide();
-		$('#profile').show();
-	},
-	edit: function() {
-		$('.page').hide();
-		$('#edit').show();
-	}
-});
+$(document).ready(function(){
+	// var user = new UserModel();
+	var App = Backbone.Router.extend({
+		routes: {
+			'': 'profile',
+			'edit': 'edit'
+		},
+		profile: function() {
+			$('.page').hide();
+			$('#profile').show();
+		},
+		edit: function() {
+			$('.page').hide();
+			$('#edit').show();
+		}
+	});
+	var app = new App();
+	Backbone.history.start();
 
-var app = new App();
-Backbone.history.start();
+	updateUser(user);
+
+	user.on("change",updateUser)
+
+	function updateUser(userModel){
+		$("#nameShow").html(user.get("name"));
+		$("#jobShow").html(user.get("role"));
+		$("#emailShow").html(user.get("email"));
+	}
+	$("#user-save").submit(function(e){
+		e.preventDefault();
+		if($("#name").val() == ""){
+			alert("Please input a name!")
+		}
+		else if($("#inputEmail3").val() == ""){
+			alert("Please input an email!")
+		}
+		else if($("#role").val() == ""){
+			alert("Please input a Job!")
+		}
+		else if($("#inputPassword3").val() == ""){
+			alert("Please input a valid password!")
+		}
+		else{
+			user.set({
+			name: $("#name").val(),
+			email: $("#inputEmail3").val(),
+			role: $("#role").val(),
+			password: $("#inputPassword3").val()
+			});
+		}	
+	});
+});
